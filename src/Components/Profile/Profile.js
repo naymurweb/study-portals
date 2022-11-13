33426c73af9e5f7865fa9693863af4c7eaf7e869
naymurweb <naymurweb@gmail.com>
2022-11-13 21:28:41 +0600

    break time add

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import profile from "../../image/profile.png";
 import "./Profile.css";
 
@@ -8,6 +8,9 @@ const Profile = ({ selectSub }) => {
     totalTime = totalTime + element.time;
   });
 
+  const [time, setTime] = useState(0);
+
+
   return (
     <div className="main">
       <div className="profile1">
@@ -15,24 +18,38 @@ const Profile = ({ selectSub }) => {
         <h3>Naymur Rahman</h3>
       </div>
       <p>
-      A Frontend Developer. I started my journey as a web developer in 2020. As well as work I also continued to keep learning and exploring new technologies for updating myself. I've made myself more efficient and productive. I love to learn new technologies and work on them. I strongly believe there is always "One More Thing" to learn.
+        A Frontend Developer. I started my journey as a web developer in 2020.
+        As well as work I also continued to keep learning and exploring new
+        technologies for updating myself. I've made myself more efficient and
+        productive. I love to learn new technologies and work on them. I
+        strongly believe there is always "One More Thing" to learn.
       </p>
 
       <div className="break">
         <h3>Add A Break </h3>
         <div className="allBtn">
-          <button className="break-btn">05m</button>
-          <button className="break-btn">10m</button>
-          <button className="break-btn">15m</button>
-          <button className="break-btn">18m</button>
-          <button className="break-btn">20m</button>
+          <button onClick={() => setTime(5)} className="break-btn">
+            05m
+          </button>
+          <button onClick={() => setTime(10)} className="break-btn">
+            10m
+          </button>
+          <button onClick={() => setTime(15)} className="break-btn">
+            15m
+          </button>
+          <button onClick={() => setTime(18)} className="break-btn">
+            18m
+          </button>
+          <button onClick={() => setTime(20)} className="break-btn">
+            20m
+          </button>
         </div>
       </div>
 
       <div className="details">
         <h3>Exercise Details</h3>
         <h4>total time: {totalTime}m</h4>
-        <h4>Break time:12m</h4>
+        <h4>Break time: {time}m</h4>
       </div>
     </div>
   );
