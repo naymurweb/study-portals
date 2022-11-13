153b0fb94566c52b3155d2f2aad6aeddec088ff8
naymurweb <naymurweb@gmail.com>
2022-11-13 20:36:55 +0600

    profile ui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Profile from "./Components/Profile/Profile";
 import Subject from "./Components/Subject/Subject";
-
+import logo from'./image/favicon.png'
 
 function App() {
   const [data, setData] = useState([]);
@@ -11,17 +11,27 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
- 
+
   return (
     <div className="main-container">
-      <div className="subject-container">
-        {
-          data.map(subject=><Subject allSubject ={subject} key={subject.id}></Subject>)
-        }
+      <div>
+        <div className="header">
+        <img src={logo} alt="" />
 
+        <h1>study portals</h1>
+        </div>
+      
 
+        <div className="subject-container">
+          {data.map((subject) => (
+            <Subject allSubject={subject} key={subject.id}></Subject>
+          ))}
+        </div>
       </div>
-      <Profile></Profile>
+
+      <div className="profile-container">
+        <Profile></Profile>
+      </div>
     </div>
   );
 }
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import profile from "../../image/profile.png";
+import "./Profile.css";
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Profile</h1>
+  return (
+    <div className="main">
+      <div className="profile1">
+        <img src={profile} alt="" />
+        <h3>Naymur Rahman</h3>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        suscipit mollitia, consequatur explicabo adipisci distinctio
+        consequuntur atque iure placeat repellendus sunt sit, eveniet, quidem
+        nihil dolor ea tempore officia quod!
+      </p>
+
+      <div className="break">
+        <h3>Add A Break </h3>
+        <div className="allBtn">
+          <button className="break-btn">05m</button>
+          <button className="break-btn">10m</button>
+          <button className="break-btn">15m</button>
+          <button className="break-btn">18m</button>
+          <button className="break-btn">20m</button>
         </div>
-    );
+      </div>
+       
+       <div className="details">
+        <h3>Exercise Details</h3>
+        <h4>Exercise time:12m</h4>
+        <h4>Break time:12m</h4>
+       </div>
+
+
+    </div>
+  );
 };
 
 export default Profile;
